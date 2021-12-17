7f4587101715d5fb973d531ad69172192d750084
aladin-jridi <aladinjridi007@gmail.com>
2021-12-17 11:47:07 +0100

    fix error login page

--- auction-house/src/app/login/login.component.ts
+++ auction-house/src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
+  templateUrl:'./login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
